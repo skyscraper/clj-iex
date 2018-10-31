601fdbe5851a18b92f2d7f7777cde66bbc1f982e
Daniel Lee <dan@spherical.nyc>
2018-10-31 09:17:01 -0400

    update project license info

--- project.clj
+++ project.clj
@@ -1,8 +1,9 @@
 (defproject clj-iex "0.1.0"
   :description "IEX API Library"
   :url "https://github.com/skyscraper/clj-iex"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "The MIT License"
+            :url "http://opensource.org/licenses/MIT"
+            :distribution :repo}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [aleph "0.4.6"]
                  [cheshire "5.8.1"]
